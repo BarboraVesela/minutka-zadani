d533f557ed6d21df7831f6895c9eead1dfca6c4f
Barbora Vesela <barbora.vesela@booking.com>
2023-03-19 21:32:38 +0100

    Hotovy ukol

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 console.log('funguju!');
+
+
+const alarm = document.querySelector('.alarm')
+const audio = document.querySelector('audio')
+const button = document.querySelector('button')
+
+let startTime = Number(prompt('Zadejte za kolik vterin ma minutka zazvonit: '))
+
+const ring = () => {
+    alarm.classList.add('.alarm--ring')
+    audio.play()
+}
+
+setTimeout(ring, startTime)
+
+const stop = () => {
+	audio.pause()
+}
+
+button.addEventListener('click', stop)
+
